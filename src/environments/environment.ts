945f60a6605ef2d1f5c3efb4a0a9e6471487d973
hugosique <otahugo02@gmail.com>
2022-10-06 00:33:35 -0300

    object teste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,83 @@ export const environment = {
   production: false
 };
 
+export const fullObject = {
+  cursos: [
+    {
+      id: 1,
+      name: 'Análise e Desenvolvimento de sistemas',
+      semestres: 4,
+      disciplinas: [
+        {
+          id: 1,
+          nome: 'POO',
+          professor: 'Aurélio',
+          semestre: 1,
+          alunos: [
+            {
+              id: 1,
+              nome: 'Hugo',
+            },
+            {
+              id: 2,
+              nome: 'Joãozinho',
+            },
+          ]
+        },
+        {
+          id: 2,
+          nome: 'Sistemas de Informação',
+          professor: 'Neto',
+          semestre: 2,
+          alunos: [
+            {
+              id: 1,
+              nome: 'Hugo',
+            },
+            {
+              id: 2,
+              nome: 'Joãozinho',
+            },
+          ]
+        },
+      ]
+    },
+    {
+      id: 2,
+      name: 'Direito',
+      semestres: 10,
+      disciplinas: [
+        {
+          id: 3,
+          nome: 'Direito Criminal',
+          professor: 'Carlos',
+          semestre: 1,
+          alunos: [
+            {
+              id: 50,
+              nome: 'Zeca',
+            },
+          ]
+        },
+      ]
+    },
+  ],
+  disciplinas: [
+    {
+      id: 1,
+      nome: 'Programação Orientada a Objetos',
+      cargaHoraria: 12,
+      professor: 'Marco Antonio',
+    },
+    {
+      id: 2,
+      nome: 'Sistemas Operacionais',
+      cargaHoraria: 12,
+      professor: 'Marco Antonio',
+    },
+  ],
+}
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
